bb339a0641d3839d9e06b1630f826a3a349c62db
root <root@DESKTOP-5UTUF1L>
2025-02-26 12:51:35 +0100

    terraform and ansible edits

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,101 @@
-resource "azurerm_resource_group" "aks" {
-  name     = var.resource_group_name
-  location = var.location
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
-  dns_prefix          = var.cluster_name
+resource "azurerm_resource_group" "rg" {
+  name     = "iac-rg"
+  location = "East US"
+}
 
-resource "null_resource" "provisioner" {
+resource "azurerm_virtual_network" "vnet" {
+  name                = "iac-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "iac-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "iac-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "iac-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "iac-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "AllowSSH"
+  priority                    = 1001
+  direction                    = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "iac-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B2s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "local_file" "inventory" {
+  content  = "[servers]\n${azurerm_linux_virtual_machine.vm.public_ip_address} ansible_user=adminuser"
+  filename = "./inventory.ini"
+}
+
+resource "null_resource" "ansible" {
+  depends_on = [azurerm_linux_virtual_machine.vm]
+
   provisioner "local-exec" {
-    command = "ansible-playbook deploy.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook.yml"
   }
-}
-
-  default_node_pool {
-    name       = "default"
-    node_count = var.node_count
-    vm_size    = "Standard_D2s_v3"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
 }
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
-
-output "aks_rg" {
-  value = azurerm_resource_group.aks.name
-}
